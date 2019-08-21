c63193776da4b0dca20432d046c408e056196593
alexmpalermo <alexmpalermo@gmail.com>
2019-08-21 12:37:56 -0400

    Updated App.js, index, and cors

--- pick6-backend/app/controllers/api/v1/users_controller.rb
+++ pick6-backend/app/controllers/api/v1/users_controller.rb
@@ -10,7 +10,8 @@ class Api::V1::UsersController < ApplicationController
 
   # GET /users/1
   def show
-    render json: @user
+    user_json = UserSerializer.new(@user).serialized_json
+    render json: user_json
   end
 
   # POST /users
--- pick6-frontend/src/App.js
+++ pick6-frontend/src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+//  componentDidMount() {
+//    fetch("http://localhost:3000/api/v1/users/1")
+//    .then(r => r.json())
+//    .then(console.log)
+//  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello i'm react</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
