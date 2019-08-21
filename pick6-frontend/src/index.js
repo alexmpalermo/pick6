02351bd1339d08acb8bddad221d5bf98160883d6
alexmpalermo <alexmpalermo@gmail.com>
2019-08-21 13:27:29 -0400

    Create store.js and move thunk and reducers into there

--- pick6-frontend/src/index.js
+++ pick6-frontend/src/index.js
@@ -3,18 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
-import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
-import users from './reducers/users.js'
+import store from './store.js'
 
-const reducer = combineReducers({
-  users
-})
-
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore( reducer, composeEnhancer(applyMiddleware(thunk)))
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
