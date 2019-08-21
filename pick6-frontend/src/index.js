2b734359ae4c3b551fbd21d6df8bfab15a209d63
alexmpalermo <alexmpalermo@gmail.com>
2019-08-21 13:33:13 -0400

    Add currentUser and add to store, change puma port to 3001

--- pick6-frontend/src/index.js
+++ pick6-frontend/src/index.js
@@ -6,7 +6,6 @@ import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux'
 import store from './store.js'
 
-
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
--- pick6-frontend/src/store.js
+++ pick6-frontend/src/store.js
@@ -2,9 +2,11 @@ import React from 'react'
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
 import thunk from 'redux-thunk'
 import users from './reducers/users.js'
+import currentUser from './reducers/currentUser.js'
 
 const reducer = combineReducers({
-  users
+  users,
+  currentUser
 })
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
