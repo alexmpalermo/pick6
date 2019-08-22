3955ccff3f7a887328dd1a5268a467a10d4bbf9e
alexmpalermo <alexmpalermo@gmail.com>
2019-08-22 18:52:27 -0400

    Add mygroups to redux and display on user show pg

--- pick6-frontend/src/components/UserShow.js
+++ pick6-frontend/src/components/UserShow.js
@@ -4,7 +4,14 @@ import { connect } from 'react-redux'
 const UserShow = (props) => {
   return (
     <div className="UserShow">
-      {props.currentUser ? <h2>Welcome, {props.currentUser.name}!</h2> : null}
+      {props.currentUser ? <h2>Welcome, {props.currentUser.name}!</h2> : null }
+      {props.currentUser && props.currentUser.groups ?
+        <ul>
+          {props.currentUser.groups.map((group) => {
+            return <li key={group.code}>{group.name}</li>
+          })}
+        </ul> :
+        null }
     </div>
   )
 }
--- pick6-frontend/src/reducers/myGroups.js
+++ pick6-frontend/src/reducers/myGroups.js
@@ -1,7 +1,7 @@
 export default (state = [], action) => {
   switch (action.type) {
     case "SET_MY_GROUPS":
-      return action.trips
+      return action.groups
     default:
       return state
   }
