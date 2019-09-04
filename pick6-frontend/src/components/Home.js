c26facbe8ded3c600b31c7511e84b25914280acb
alexmpalermo <alexmpalermo@gmail.com>
2019-09-04 16:18:01 -0400

    Add links to signup/login to navbar and add description to home

--- pick6-frontend/src/components/Home.js
+++ pick6-frontend/src/components/Home.js
@@ -7,9 +7,13 @@ import Signup from './Signup.js'
 
 const Home = () => (
   <div>
+    <h3>The football spreadsheet platform for weekly bets against friends</h3>
     <span>
       <p><Link to="/signup">Sign Up</Link></p> or <p><Link to="/login">Log In</Link></p>
     </span>
+    <footer>
+      This app is not affiliated with, funded by, or in any way associated with the NFL.
+    </footer>
   </div>
 
 );
--- pick6-frontend/src/components/Navbar.js
+++ pick6-frontend/src/components/Navbar.js
@@ -3,11 +3,15 @@ import { connect } from 'react-redux'
 import { NavLink } from 'react-router-dom'
 import Login from './Login.js'
 import Logout from './Logout.js'
+import Signup from './Signup.js'
 
 const Navbar = (props) => {
   return (
     <div className="Navbar">
       <h1>Pick6</h1>
+      {props.location.pathname === "/login" ? <NavLink to="/signup">Sign Up</NavLink> : null}
+      {props.location.pathname === "/signup" ? <NavLink to="/login">Log In</NavLink> : null}
+      <br/>
       {props.currentUser ? <NavLink to="/groups/new">New Group</NavLink> : null}
       <br/>
       {props.currentUser ? <NavLink to="/">Home</NavLink> : null}
