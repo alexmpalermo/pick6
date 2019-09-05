328f09ae89d3f5a1da32beb2170f92c78d52323f
alexmpalermo <alexmpalermo@gmail.com>
2019-09-05 15:22:10 -0400

    Add actin/reducer for get my weeks, also add WeekShow component file

---
+++ pick6-frontend/src/actions/myWeeks.js
@@ -0,0 +1,29 @@
+// sync
+
+export const setMyWeeks = weeks => {
+  return {
+    type: "SET_MY_WEEKS",
+    weeks
+  }
+}
+
+// async
+
+export const getMyWeeks = () => {
+  return dispatch => {
+    return fetch("http://localhost:3001/api/v1/weeks", {
+      credentials: "include",
+      method: "GET",
+      headers: { "Content-Type": "application/json" }
+    })
+    .then(r => r.json())
+    .then(resp => {
+      if (resp.error) {
+        alert(resp.error)
+      } else {
+        dispatch(setMyWeeks(resp.data))
+      }
+    })
+    .catch(console.log)
+  }
+}
