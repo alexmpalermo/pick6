900c34356268590c46deed3fd87ee920fc6f9860
alexmpalermo <alexmpalermo@gmail.com>
2019-09-06 15:47:36 -0400

    Fix week show into a conditional

--- pick6-frontend/src/components/WeekShow.js
+++ pick6-frontend/src/components/WeekShow.js
@@ -12,10 +12,13 @@ class WeekShow extends React.Component {
   render(){
     console.log(this.props)
     const { week } = this.props
+
     return (
-      <div>
-        <h3>WEEK ??</h3>
-      </div>
+      week ?
+      <><div>
+        <h3>WEEK {week.attributes.number}</h3>
+      </div></> :
+      null
     )
   }
 
