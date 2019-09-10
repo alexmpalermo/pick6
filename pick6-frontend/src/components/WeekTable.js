024e9f33b6d3071957bc2162b84c0cebf63d6fa0
alexmpalermo <alexmpalermo@gmail.com>
2019-09-10 19:04:37 -0400

    New table for week show

--- pick6-frontend/src/components/WeekTable.js
+++ pick6-frontend/src/components/WeekTable.js
@@ -1,33 +1,59 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-const WeekTable = ({week}) => {
+const WeekTable = ({week, user}) => {
 
   return (
     <>
+    <div>
+      <h2>WEEK {week.attributes.number} SPREADSHEET</h2>
       <div>
         <table>
-          <thead>
-          <tr>
-            <th>Name</th>
-            <th>Lastname</th>
-            <th>Age</th>
-          </tr>
-          </thead>
           <tbody>
           <tr>
+            <td><strong>HOME</strong></td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>50</td>
+            <td>Jill</td>
+            <td>Smith</td>
+            <td>50</td>
+            <td>Jill</td>
+            <td>Smith</td>
+            <td>50</td>
+            <td>Smith</td>
+            <td>50</td>
             <td>Jill</td>
             <td>Smith</td>
             <td>50</td>
           </tr>
           <tr>
-            <td>Eve</td>
+            <td><strong>SPREAD</strong></td>
             <td>Jackson</td>
             <td>94</td>
           </tr>
+          <tr>
+            <td><strong>AWAY</strong></td>
+            <td>Jackson</td>
+            <td>94</td>
+          </tr>
+          <tr>
+            <td><strong>WINNING TEAM</strong></td>
+            <td>Jackson</td>
+            <td>94</td>
+          </tr>
+          <tr>
+            <td><strong>NAME</strong></td>
+            <td>Jackson</td>
+            <td>94</td>
+          </tr>
+          
           </tbody>
         </table>
       </div>
+    </div>
     </>
   )
 }
