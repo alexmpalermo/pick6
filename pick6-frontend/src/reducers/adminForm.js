b15d204a283ac7153e3de54169e91a7fa7cebd98
alexmpalermo <alexmpalermo@gmail.com>
2019-09-13 15:34:43 -0400

    Add reducer for games, adminform

---
+++ pick6-frontend/src/reducers/adminForm.js
@@ -0,0 +1,27 @@
+const initialState = {
+  home: "",
+  away: "",
+  hscore: "",
+  ascore: "",
+  handicap: "",
+  total: "",
+  day: "",
+  time: ""
+}
+
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case "UPDATE_ADMIN_FORM":
+      return {
+        ...state,
+        [action.formData.name]: action.formData.value
+      }
+    case "RESET_ADMIN_FORM":
+      return initialState
+    case "SET_ADMIN_FORM_DATA":
+      return action.gameFormData
+    default:
+      return state
+  }
+}
--- pick6-frontend/src/reducers/games.js
+++ pick6-frontend/src/reducers/games.js
@@ -4,6 +4,8 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case "SET_MY_GAMES":
       return action.games
+    case "UPDATE_GAME":
+      return state.map(game => game.id === action.game.id ? action.game : game)
     default:
       return state
   }
