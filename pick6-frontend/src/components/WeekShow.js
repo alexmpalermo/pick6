1093d198352b46f0dd8e0b6ec13e7809d423f44b
alexmpalermo <alexmpalermo@gmail.com>
2019-09-13 18:15:51 -0400

    Add conditional so if the date is too late it shows table instead

--- pick6-frontend/src/components/WeekShow.js
+++ pick6-frontend/src/components/WeekShow.js
@@ -4,6 +4,7 @@ import { getCurrentWeek } from '../actions/currentWeek.js'
 import WeekTable from './WeekTable.js'
 import PickSheetForm from './PickSheetForm.js'
 import MyPicks from './MyPicks.js'
+import moment from "moment";
 
 class WeekShow extends React.Component {
 
@@ -14,16 +15,33 @@ class WeekShow extends React.Component {
 
 
   render(){
-
     const { week, currentUser } = this.props
+    const today = moment().format('YYYY-MM-DD')
+    const showSomething = () => {
+      if (week) {
+        if (currentUser && currentUser.attributes.picks.find(p => p.week_id === parseInt(week.id))) {
+          if (today > week.attributes.games[0].day || today === week.attributes.games[0].day){
+            return <WeekTable week={week} />
+          } else {
+            return <MyPicks week={week} />
 
+          }
+        } else {
+          return <PickSheetForm week={week}/>
+        }
+      } else {
+        return null
+      }
+    }
+
+    const idk = week ? console.log("day", moment().format('YYYY-MM-DD'), week.attributes.games[0].day) : null
     return (
       week ?
       <><div>
         <h3>WEEK {week.attributes.number}</h3>
-        {currentUser && currentUser.attributes.picks.find(p => p.week_id === parseInt(week.id)) ? <MyPicks week={week} /> : <PickSheetForm week={week}/>}
+        <span>{showSomething()}</span>
         <h4>Show full table with everyones picks if week has started</h4>
-        <WeekTable week={week} />
+
       </div></> :
       null
     )
