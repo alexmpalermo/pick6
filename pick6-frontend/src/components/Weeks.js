d54fab5ba2df8035a52d1346ac523e0c36cc68d8
alexmpalermo <alexmpalermo@gmail.com>
2019-09-23 19:09:20 -0400

    Edit a little bit - trying to show current week

--- pick6-frontend/src/components/Weeks.js
+++ pick6-frontend/src/components/Weeks.js
@@ -4,21 +4,21 @@ import moment from "moment";
 
 const Weeks = ({ group, weeks }) => {
   const today = moment().format('YYYY-MM-DD')
-  const currWeek = weeks.length > 0 ? weeks.find(week => {
-    if (week.attributes.games > 0) {
-      return ((today > week.attributes.games[0].day && today < week.attributes.games[-1].day))
-    } else {
-      return null
-    }
+  // const currWeek = weeks.length > 0 && weeks[0].attributes.games.length > 0 ? weeks.find(week => today > week.attributes.games[0].day && today < week.attributes.games[-1].day) : null
 
-  }) : null
+  const currWeek = () => {
+    const theWeek = weeks.find(week => (today > week.attributes.games[0].day && today < week.attributes.games[-1].day))
+    console.log(theWeek)
+    return (theWeek)
+  }
 
-console.log(weeks)
+  console.log(weeks.length > 0 ? weeks : null)
+  console.log(currWeek())
 
   return (
     weeks.length > 0 ?
       <div className="week-list">
-        {currWeek > 0 ? <h3>Current Week: <Link to={`/weeks/${currWeek.id}`}> Week {currWeek.attributes.number}</Link></h3> : null}
+        {weeks[-1] ? currWeek().attributes.number : null}
         {weeks.map((week) => {
           return <p key={week.id} className="week-name"><Link to={`/weeks/${week.id}`}> Week {week.attributes.number}</Link></p>
           }
@@ -30,5 +30,5 @@ console.log(weeks)
 
 export default Weeks
 
-//
+// {currWeek > 0 ? <h3>Current Week: <Link to={`/weeks/${currWeek.id}`}> Week {currWeek.attributes.number}</Link></h3> : null}
 //
