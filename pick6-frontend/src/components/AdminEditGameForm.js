9016f19a77ed6829ffad4faf81f6b094ec7cf5ee
alexmpalermo <alexmpalermo@gmail.com>
2019-09-24 16:58:51 -0400

    Add css for admin edit game form

--- pick6-frontend/src/components/AdminEditGameForm.js
+++ pick6-frontend/src/components/AdminEditGameForm.js
@@ -34,15 +34,16 @@ class AdminEditGameForm extends React.Component {
 
     return (
       <>
+      <h1 className="page-title">Edit This Game: </h1>
       <form onSubmit={this.handleSubmit}>
-        <input placeholder="home" type="text" name="home" value={formData.home} onChange={this.handleChange} /><br/>
-        <input placeholder="away" type="text" name="away" value={formData.away} onChange={this.handleChange} /><br/>
-        <input placeholder="winner" type="text" name="winner" value={formData.winner} onChange={this.handleChange} /><br/>
-        <input placeholder="handicap" type="number" name="handicap" value={formData.handicap} onChange={this.handleChange} /><br/>
-        <input placeholder="total" type="number" min="0" name="total" value={formData.total} onChange={this.handleChange} /><br/>
-        <input placeholder="2019-09-05" type="text" name="day" value={formData.day} onChange={this.handleChange} /><br/>
-        <input placeholder="13:30" type="text" name="time" value={formData.time} onChange={this.handleChange} /><br/>
-        <input type="submit" value="Edit Game" />
+        <input placeholder="home" type="text" name="home" value={formData.home} onChange={this.handleChange} className="input"/><br/>
+        <input placeholder="away" type="text" name="away" value={formData.away} onChange={this.handleChange} className="input"/><br/>
+        <input placeholder="winner" type="text" name="winner" value={formData.winner} onChange={this.handleChange} className="input"/><br/>
+        <input placeholder="handicap" type="number" name="handicap" value={formData.handicap} onChange={this.handleChange} className="input"/><br/>
+        <input placeholder="total" type="number" min="0" name="total" value={formData.total} onChange={this.handleChange} className="input"/><br/>
+        <input placeholder="2019-09-05" type="text" name="day" value={formData.day} onChange={this.handleChange} className="input"/><br/>
+        <input placeholder="13:30" type="text" name="time" value={formData.time} onChange={this.handleChange} className="input"/><br/><br/>
+        <input type="submit" value="Edit Game" className="input"/>
       </form>
       </>
     )
