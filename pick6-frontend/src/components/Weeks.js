09bfd80c23181b72f72f9f217110815b8c547a63
alexmpalermo <alexmpalermo@gmail.com>
2019-09-24 13:47:22 -0400

    Remove comments and consolelogs

--- pick6-frontend/src/components/Weeks.js
+++ pick6-frontend/src/components/Weeks.js
@@ -3,47 +3,8 @@ import { Link } from 'react-router-dom'
 import moment from "moment";
 
 const Weeks = ({ group, weeks }) => {
+
   const today = moment().format('YYYY-MM-DD')
-  // const currWeek = weeks.length > 0 && weeks[0].attributes.games.length > 0 ? weeks.find(week => today > week.attributes.games[0].day && today < week.attributes.games[-1].day) : null
-
-  // const currWeek = () => {
-  //  if (weeks.length > 0) {
-  //   const finalGame = weeks[0].attributes.games[weeks[0].attributes.games.length - 1]
-  //   console.log("today", today)
-  //   console.log("final game", finalGame.day)
-  //   console.log("today >= final", today >= finalGame.day)
-  //   console.log("today <= final", today <= finalGame.day)
-  //   return finalGame
-  // } else {
-  //   return null
-  // }}
-  //
-  // console.log(currWeek())
-
-  //     // console.log("final game", finalGame.day)
-  //     // console.log("today", today)
-  //     // console.log("weeks", weeks)
-  //     // console.log("is ture?", today >= finalGame.day)
-  //     // console.log("is false...", today <= finalGame.day)
-  //     // console.log("true??", today <= today)
-  //     // console.log("true??", today >= today)
-  //     // console.log("find...", weeks.find(week => week.attributes.games[0].day <= today <= week.attributes.games[week.attributes.games.length - 1].day))
-  //     // console.log("find2...", weeks.find(week => today <= week.attributes.games[week.attributes.games.length - 1].day))
-  //     const nextWeek = weeks.find(week => today <= week.attributes.games[week.attributes.games.length - 1].day)
-  //     weeks.map(week => {
-  //       if (today >= week.attributes.games[0].day && today <= week.attributes.games[week.attributes.games.length - 1].day) {
-  //         return week
-  //       } else {
-  //         return null
-  //       }
-  //     })
-  //
-  //     console.log(nextWeek)
-  //     return (nextWeek)
-  //   } else {
-  //     return null
-  //   }
-  // }
 
   const thisWeek = weeks.filter(week => {
     return today > week.attributes.games[week.attributes.games.length - 1].day
@@ -56,27 +17,6 @@ const Weeks = ({ group, weeks }) => {
       return thisWeek.length + 1
     }
   }
-
-
-
-
-
-
-
-
-  //   {
-  //   console.log(today)
-  //   if (today >= week.attributes.games[0].day && today <= week.attributes.games[week.attributes.games.length - 1].day) {
-  //     return week
-  //   } else {
-  //     return null
-  //   }
-  // })
-
-  console.log(thisWeek)
-  console.log(actualWeekNumber())
-
-
 
   return (
     weeks.length > 0 ?
@@ -92,5 +32,3 @@ const Weeks = ({ group, weeks }) => {
 }
 
 export default Weeks
-
-//
