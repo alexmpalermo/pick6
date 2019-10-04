9ec9eacf59ea3331fe15b1964558897534983ab8
alexmpalermo <alexmpalermo@gmail.com>
2019-10-04 14:59:14 -0400

    Add code to make a nunber 1 button (project review)

--- pick6-frontend/src/components/Groups.js
+++ pick6-frontend/src/components/Groups.js
@@ -3,13 +3,34 @@ import { Link } from 'react-router-dom'
 
 
 const Groups = ({ groups }) => {
+  // const num = () => {
+  //   if (activateNumber()) {
+  //     return 1
+  //   } else {
+  //     return 0
+  //   }
+  // }
+// thinking in react
+// the promise of a burger party
+
+  const activateNumber = (e) => {
+    e.preventDefault();
+    console.log('The link was clicked.');
+
+  }
 
   return (
     groups.length > 0 ?
       <div>
         <><h2><u>My Groups:</u></h2>
         {groups.map((group) => {
-          return (<p key={group.id} className="group-links"><Link to={`/groups/${group.id}`} >{group.attributes.name}</Link></p>)
+          return (
+            <p key={group.id} className="group-links">
+              <Link to={`/groups/${group.id}`} >{group.attributes.name}</Link>
+              ----<button onClick={activateNumber}>Click to make it #1</button>
+
+            </p>
+          )
           }
         )}
         </>
